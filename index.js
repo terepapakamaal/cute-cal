ee9125711243e7d7320e605a779d273963708f45
Time_Traveller <152459170+terepapakamaal@users.noreply.github.com>
2025-04-03 18:30:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
 const display = document.getElementById("display");
 
 function deleteLast() {
-    display.value = display.value.slice(0, -1)
+    display.value = display.value.slice(0, -1);
 }
 
 function appendToDisplay(input) {
     display.value += input;
-
 }
+
 function clearDisplay() {
     display.value = "";
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value);
-    }
-    catch (error) {
+        // Replace % with /100 to calculate percentage
+        display.value = eval(display.value.replace(/%/g, '/100'));
+    } catch (error) {
         display.value = "error";
     }
 }
+
 document.addEventListener("keydown", (event) => {
-    if (/[\d+\-*/.=]/.test(event.key)) {
-        if (event.key === "=") {
+    if (/[\d+\-*/.%=]/.test(event.key)) {
+        if (event.key === "=" || event.key === "Enter") {
             calculate();
-
-        }
-        else {
+        } else {
             appendToDisplay(event.key);
-
         }
-    }
-    else if (event.key === "Enter") {
-        calculate();
-
-    }
-    else if (event.key === "Backspace") {
+    } else if (event.key === "Backspace") {
         deleteLast();
-
-    }
-    else if (event.key === "Escape") {
+    } else if (event.key === "Escape") {
         clearDisplay();
-
     }
 });
